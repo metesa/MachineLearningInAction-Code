99870c6309fd58f682de2390bb5eca34e940cd06
metesa <submetesame@gmail.com>
2017-07-03 01:10:24 +0800

    Progress: P37

--- Ch3/trees.py
+++ Ch3/trees.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # Part 1 - Chapter 3 - Decision Tree
 
-
 from math import log
 
 
@@ -20,3 +19,23 @@ def calculate_shannon_entropy(dataset):
         probably = float(label_counts[key]) / entry_count
         shannon_entropy -= probably * log(probably, 2)
     return shannon_entropy
+
+
+def create_dataset():
+    dataset = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataset, labels
+
+
+def split_dataset(dataset, axis, value):
+    ret_dataset = []
+    for feat_vec in dataset:
+        if feat_vec[axis] == value:
+            reduced_feat_vec = feat_vec[:axis]
+            reduced_feat_vec.extend(feat_vec[axis+1:])
+            ret_dataset.append(reduced_feat_vec)
+    return ret_dataset
